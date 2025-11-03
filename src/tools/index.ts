6b916956c756166bf6545b354e428380af0a49f3
KK <k.kadatoka@gmail.com>
2025-11-03 14:23:15 +1100

    Register search tool in MCP

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,17 +1,21 @@
 import { fetchUrlTool, fetchUrl } from './fetchUrl.js';
 import { fetchUrlsTool, fetchUrls } from './fetchUrls.js';
 import { browserInstallTool, browserInstall } from './browserInstall.js';
+import { searchGoogleTool, searchGoogle } from './searchGoogle.js';
 
 // Export tool definitions
 export const tools = [
   fetchUrlTool,
   fetchUrlsTool,
-  browserInstallTool
+  browserInstallTool,
+  searchGoogleTool
 ];
 
 // Export tool implementations
 export const toolHandlers = {
   [fetchUrlTool.name]: fetchUrl,
   [fetchUrlsTool.name]: fetchUrls,
-  [browserInstallTool.name]: browserInstall
+  [browserInstallTool.name]: browserInstall,
+  [searchGoogleTool.name]: searchGoogle
 };
+
